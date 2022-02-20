feff7bee2819d5c9dff55f46d6dfe1c396e9f86c
CaioMassula <96846073+CaioMassula@users.noreply.github.com>
2022-02-20 09:46:29 -0300

    Add files via upload

---
+++ project-C22-template-mainpt-main/sketch.js
@@ -0,0 +1,52 @@
+//Tem algo de errado que não consegui indentifica
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var baseimage,playerimage;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
+
+  var options = {
+  isStatic: true
+  };
+  //criar corpo da base do jogador
+  playerBase = Bodies.rectangle(200,350,180,150,options);
+  World.add(world,playerBase);
+  //criar corpo do jogador
+  player = Bodies.rectangle(250,playerBase.position.y - 160,50,180,options);
+  World.add(world,player);
+
+
+}
+
+function draw() {
+  background(backgroundImg),0,0,1200,600;
+
+  //exibir a imagem do jogador usando a função image()
+  image(baseimage,playerBase.position.x,PlayerBase.position.y,180,150);
+  //exibir a imagem da base do jogador usando a função image()
+  image(playerimage,player.position.x,Player.position.y,50,180);
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("ARQUEIRO ÉPICO", width / 2, 100);
+}
